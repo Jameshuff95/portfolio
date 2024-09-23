9309ab54bcba0750717382ac16de8b87bc8c6b29
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-09-23 03:48:51 -0400

    changed styling

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,4 +1,5 @@
 import '../CSS/App.css';
+import Me from '../assets/me3.png';
 import Accordion from './Accordion';
 
 function About() {
@@ -7,7 +8,17 @@ function About() {
       <h4>Learn More About Me</h4>
       <Accordion
         title="Background"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        content={
+          <div id="background_info">
+            <img src={Me} alt="James Huff" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        }
       />
       <Accordion
         title="Motivations"
