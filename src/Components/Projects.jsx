df180e86357dc7a5bfa8b39051e40cd2242a7d44
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-09-26 14:09:31 -0400

    .

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,84 +5,84 @@ import React, { useState } from 'react';
 function Projects({ isDark }) {
   const projects = [
     {
-      id: 1,
+      id: '1',
       title: 'React Project 1',
       description: 'React Description 1',
       img: '#',
       category: 'react',
     },
     {
-      id: 2,
+      id: '2',
       title: 'React Project 2',
       description: 'React Description 2',
       img: '#',
       category: 'react',
     },
     {
-      id: 3,
+      id: '3',
       title: 'React Project 3',
       description: 'React Description 3',
       img: '#',
       category: 'react',
     },
     {
-      id: 4,
+      id: '4',
       title: 'C++ Project 1',
       description: 'C++ Description 1',
       img: '#',
       category: 'c++',
     },
     {
-      id: 5,
+      id: '5',
       title: 'C++ Project 2',
       description: 'C++ Description 2',
       img: '#',
       category: 'c++',
     },
     {
-      id: 6,
+      id: '6',
       title: 'C++ Project 3',
       description: 'C++ Description 3',
       img: '#',
       category: 'c++',
     },
     {
-      id: 7,
+      id: '7',
       title: 'SQL Project 1',
       description: 'SQL Description 1',
       img: '#',
       category: 'sql',
     },
     {
-      id: 8,
+      id: '8',
       title: 'SQL Project 2',
       description: 'SQL Description 2',
       img: '#',
       category: 'sql',
     },
     {
-      id: 9,
+      id: '9',
       title: 'SQL Project 3',
       description: 'SQL Description 3',
       img: '#',
       category: 'sql',
     },
     {
-      id: 10,
+      id: '10',
       title: 'Python Project 1',
       description: 'Python Description 1',
       img: '#',
       category: 'python',
     },
     {
-      id: 11,
+      id: '11',
       title: 'Python Project 2',
       description: 'Python Description 2',
       img: '#',
       category: 'python',
     },
     {
-      id: 12,
+      id: '12',
       title: 'Python Project 3',
       description: 'Python Description 3',
       img: '#',
@@ -91,6 +91,12 @@ function Projects({ isDark }) {
   ];
 
   const [filteredProjects, setFilteredProjects] = useState(projects);
+  const [selectedImage, setSelectedImage] = useState('');
+
+  function handleSelectImage(event) {
+    const newSelectedImage = event.target.id;
+    setSelectedImage(newSelectedImage);
+  }
 
   function filterOptions(event) {
     const category = event.target.value;
@@ -131,15 +137,27 @@ function Projects({ isDark }) {
             key={project.id}
           >
             <div
-              className={`${
-                isDark
-                  ? 'project_image dark-theme'
-                  : 'project_image light-theme'
-              }`}
-            >
-              <button className="project_button">Go To Repository</button>
-              <button className="project_button">Go to site</button>
-            </div>
+              className="thumbnail"
+              id={project.id}
+              onClick={handleSelectImage}
+              style={{
+                display: selectedImage === project.id ? 'none' : 'block',
+                backgroundImage: `url(${project.img})`,
+                backgroundSize: 'cover',
+              }}
+            />
+            {selectedImage === project.id && (
+              <div
+                className={`${
+                  isDark
+                    ? 'project_image dark-theme'
+                    : 'project_image light-theme'
+                }`}
+              >
+                <button className="project_button">Go To Repository</button>
+                <button className="project_button">Go to site</button>
+              </div>
+            )}
             <h1>{project.title}</h1>
             <p>{project.description}</p>
           </div>
